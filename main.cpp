93cd878f2930a5473fd2ba7934394ecad56c3014
Denzov <podlesniy0209@gmail.com>
2025-02-05 09:56:56 +0300

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <filesystem>
 
 int main() {
-    system("g++ mainUI.cpp UI.cpp UserCode/decoder.cpp UserCode/encoder.cpp -o UI");
-    system("UI");
-    system("del UI.exe");
+    std::string user_code_path = "UserUI/";
+    std::string program_name = "PROCESS_UI";
+
+    system(("g++ " + user_code_path + "*.cpp -o " + program_name).c_str());
+    if(std::filesystem::exists(program_name + ".exe"))
+    {
+        system(("attrib +h " + program_name + ".exe").c_str());
+        system(program_name.c_str());
+        system(("attrib -h " + program_name + ".exe").c_str());
+        system(("del " + program_name + ".exe").c_str());
+    }
     return 0;
 }
